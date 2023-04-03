243e1df78e1ffb6fe38faad707a1d072e843e50b
Aleksandr <alexlev83@list.ru>
2023-04-03 15:05:42 +0300

    Задача 25

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 1. семинар 4  Напишите программу, которая принимает 
+// на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+// int GetSum(int num)
+// {
+//   int sum = 0;
+//   for(int current = 1; current <= num; current++)
+//     sum = sum + current;
+//   return sum;
+
+// }
+// Console.Write("input a number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int res = GetSum(a);
+// Console.WriteLine($"sum of number from 1 to {a} is {res}");
+
+//Напишите программу, которая принимает на вход число и выдаёт 
+//количество цифр в числе.
+
+
+
+// Напишите программу, которая принимает на вход число N и выдаёт 
+// произведение чисел от 1 до N.
+// Task 3. Напишите программу, 
+//которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+// int GetSum(int num)
+// {
+//     int sum = 0;
+
+//     for(int current = 1; current <= num; current++)
+//         sum = sum * current;
+//     return sum;
+// }
+// Console.Write("Input number A: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// int res = GetSum(A);
+// Console.WriteLine($"Sum of numbers from 1 do {A} is {res}");
+
